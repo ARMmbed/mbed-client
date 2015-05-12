952f8cc01b63c432cc7ea38a486383504edc0b32
Yogesh Pande <Yogesh.Pande@arm.com>
2015-05-12 09:42:03 +0300

    Adding private tag to module.json and updating some build files for linux builds.

--- include_dirs.mk
+++ include_dirs.mk
@@ -1,5 +1,9 @@
 INCLUDE_DIRS := \
 	../ \
 	. \
-	../nsdl-c/nsdl-c \
+	../nsdl-c \
 	../libService/libService \
+        lwm2m-client-linux \
+        lwm2m-client-linux/source \
+        source \
+
