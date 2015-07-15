c72a028a4945dceb149260bac4d35e4a8a9860ea
Yogesh Pande <Yogesh.Pande@arm.com>
2015-07-15 19:23:12 +0300

    Fix for building lwm2m-client on all possible build configuration
    Supported build configurations
     - Yotta Linux
     - Yotta mbed
     - Makefile for Linux

--- lwm2m-client/m2mconnectionhandler.h
+++ lwm2m-client/m2mconnectionhandler.h
@@ -9,7 +9,7 @@
 #include "lwm2m-client/m2minterface.h"
 
 //TODO: refactor this to include lwm2m-client header!
-#include "sn_nsdl.h"
+#include "nsdl-c/sn_nsdl.h"
 
 class M2MConnectionSecurity;
 class M2MConnectionHandlerPimpl;
