6253ebc6014fe607f4bd2d1b82789fb81137657c
Yogesh Pande <Yogesh.Pande@arm.com>
2015-07-17 16:45:36 +0300

    Updating include_dirs.mk for internal builds.
    version v0.1.13

--- include_dirs.mk
+++ include_dirs.mk
@@ -3,6 +3,7 @@ INCLUDE_DIRS := \
 	. \
 	../../nsdl-c \
 	../../libService/libService \
+	../../libService/exported-libs/mbed-client-libservice \
 	../../libService \
 	../../mbedtls/include \
         ../lwm2m-client-linux \
