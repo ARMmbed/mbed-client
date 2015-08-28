d87167e09a84d24e7ee845d8433eaf2fbed724ef
Yogesh Pande <Yogesh.Pande@arm.com>
2015-08-28 16:34:44 +0300

    Fix for right license text.

--- test/mbedclient/utest/common/mbed-client-libservice/ns_trace.h
+++ test/mbedclient/utest/common/mbed-client-libservice/ns_trace.h
@@ -1,22 +1,19 @@
-/**
- * Copyright ARM 2014
- * \file ns_trace.h
- * \brief Trace interface for NanoStack library.
+/*
+ * Copyright (c) 2015 ARM Limited. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ * Licensed under the Apache License, Version 2.0 (the License); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * \note
- *  usage:
- *      myfile.c:
- *      #include "ns_trace.h"
- *      #define TRACE_GROUP  "mylb"
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *      void myfuction(){
- *          tr_debug("this is debug msg");
- *          tr_err("this is error msg");
- *          tr_warn("this is warning msg");
- *          tr_info("this is info msg");
- *      }
- *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 #ifndef NS_TRACE_H_
 #define NS_TRACE_H_
 #include "ns_types.h"
