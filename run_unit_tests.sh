ab284f8c66b6da8e021b03229fd88409de5e4357
Antti Yli-Tokola <antti.yli-tokola@arm.com>
2015-09-25 08:39:51 +0300

    Skip test coverage from yotta_modules

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -37,21 +37,19 @@ mkdir results
 mkdir coverage
 
 find ./ -name '*.xml' | xargs cp -t ./results/
-find ./ -name '*.gcno' | xargs cp -t ./coverage/
-find ./ -name '*.gcda' | xargs cp -t ./coverage/
+find ./build -name '*.gcno' | xargs cp -t ./coverage/
+find ./build -name '*.gcda' | xargs cp -t ./coverage/
 exclude_files="${PWD}/test/"
 gcovr --object-directory ./coverage --exclude '/usr' --exclude $exclude_files  -x -o ./results/gcovr.xml
-rm -f ./coverage/*.gcno
-rm -f ./coverage/*.gcda
-
 echo
 echo "Create coverage document"
 echo
-lcov -b ./ -d ./ -c -o ./coverage/app.info
+lcov -b ./ -d ./coverage -c -o ./coverage/app.info
 lcov -q -r ./coverage/app.info "/test/mbedclient/*" -o ./coverage/app.info
 lcov -q -r ./coverage/app.info "/usr*" -o ./coverage/app.info
 genhtml -q --no-branch-coverage --function-coverage --demangle-cpp --title "mbed Client coverage" ./coverage/app.info -o ./coverage
-
+rm -f ./coverage/*.gcno
+rm -f ./coverage/*.gcda
 echo 
 echo "Creating report"
 echo 
