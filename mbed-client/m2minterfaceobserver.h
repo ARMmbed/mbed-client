7fb37485cf85a0d8408eb3540e9302521970635a
Pekka Niskanen <pekka.niskanen@arm.com>
2016-02-23 13:52:09 +0200

    editing documentation

--- mbed-client/m2minterfaceobserver.h
+++ mbed-client/m2minterfaceobserver.h
@@ -23,7 +23,7 @@
 class M2MServer;
 
 /**
- * @brief M2MInterfaceObserver
+ * \brief M2MInterfaceObserver
  * This is an observer class that updates the calling application about
  * various events associated with various Interface operations.
  * Also, it informs about various errors that can occur during any of the above
@@ -34,51 +34,51 @@ class M2MInterfaceObserver {
 public:
 
     /**
-     * @brief Callback indicating that the bootstap has been done successfully.
-     * @param server_object Server object that contains information fetched
+     * \brief A callback indicating that the bootstap has been performed successfully.
+     * \param server_object The server object that contains information fetched
      * about the LWM2M server from the bootstrap server. This object can be used
      * to register to the LWM2M server. The object ownership is passed.
      */
     virtual void bootstrap_done(M2MSecurity *server_object) = 0;
 
     /**
-     * @brief Callback indicating that the device object has been registered
+     * \brief A callback indicating that the device object has been registered
      * successfully to the LWM2M server.
-     * @param security_object Server object on which the device object is
+     * \param security_object The server object on which the device object is
      * registered. The object ownership is passed.
-     * @param server_object, Object containing information about the LWM2M server.
+     * \param server_object An object containing information about the LWM2M server.
      * The client maintains the object.
      */
     virtual void object_registered(M2MSecurity *security_object, const M2MServer &server_object) = 0;
 
     /**
-     * @brief Callback indicating that the device object has been successfully unregistered
+     * \brief A callback indicating that the device object has been successfully unregistered
      * from the LWM2M server.
-     * @param server_object Server object from which the device object is
+     * \param server_object The server object from which the device object is
      * unregistered. The object ownership is passed.
      */
     virtual void object_unregistered(M2MSecurity *server_object) = 0;
 
     /**
-     * @brief Callback indicating that the device object registration has been successfully
+     * \brief A callback indicating that the device object registration has been successfully
      * updated on the LWM2M server.
-     * @param security_object Server object on which the device object registration
+     * \param security_object The server object on which the device object registration
      * updated. The object ownership is passed.
-     * @param server_object, Object containing information about the LWM2M server.
+     * \param server_object An object containing information about the LWM2M server.
      * The client maintains the object.
      */
     virtual void registration_updated(M2MSecurity *security_object, const M2MServer & server_object) = 0;
 
     /**
-     * @brief Callback indicating that there is an error during the operation.
-     * @param error Error code informing about the error that occured.
+     * \brief A callback indicating that there was an error during the operation.
+     * \param error An error code for the occurred error.
      */
     virtual void error(M2MInterface::Error error) = 0;
 
     /**
-     * @brief Callback indicating that the value of the resource object is updated by the server.
-     * @param base Object whose value is updated.
-     * @param type Type of object.
+     * \brief A callback indicating that the value of the resource object is updated by the server.
+     * \param base The object whose value is updated.
+     * \param type The type of the object.
      */
     virtual void value_updated(M2MBase *base, M2MBase::BaseType type) = 0;
 };
