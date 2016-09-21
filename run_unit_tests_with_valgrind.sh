f9dd69af9d3f6612b30e0062b3fe9a6d0bb5f53c
Janne Kiiskilä <janne.kiiskila@arm.com>
2016-09-21 04:23:34 -0700

    Apache 2.0 license additions
    Plus some year fixings to the copyright note to keep up-to-date.

--- run_unit_tests_with_valgrind.sh
+++ run_unit_tests_with_valgrind.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 # Execute script with root path where to find binaries. 
 # For example ./run_unit_tests_with_valgrind.sh ./build/x86-linux-native-coverage/test/mbedclient/
+#
+# Copyright (c) 2015-2016 ARM Limited. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the License); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# * http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an AS IS BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 if ./tools_check.sh
 then
