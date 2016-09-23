ARMmbed/mbed-client

commit 4f6326ee2819a74ae18a5532578694a0359058b1

Introduce new API to publish new objects to lwm2m server as a part of … (#288)
* Introduce new API to publish new object to lwm2m server as a part of registration update.

* Fix memory leak in M2MInterfaceImpl::start_register_update.
Update unit tests.

source/m2mnsdlinterface.cpp
@@ -298,7 +298,6 @@ bool M2MNsdlInterface::send_update_registration(const uint32_t lifetime)
     tr_debug("M2MNsdlInterface::send_update_registration( lifetime %" PRIu32 ")", lifetime);
     bool success = false;
 
-    create_nsdl_list_structure(_object_list);
     //If Lifetime value is 0, then don't change the existing lifetime value
     if(lifetime != 0) {
         if(_endpoint->lifetime_ptr) {
@@ -663,7 +662,7 @@ uint8_t M2MNsdlInterface::resource_callback(struct nsdl_s */*nsdl_handle*/,
             coap_response->coap_status != COAP_STATUS_PARSER_BLOCKWISE_MSG_RECEIVING) {
         value_updated(base,base->uri_path());
     }
-    
+
     sn_nsdl_release_allocated_coap_msg_mem(_nsdl_handle, coap_response);
     return result;
 }

mbed-client/m2minterface.h
@@ -135,6 +135,20 @@ public:
     virtual void update_registration(M2MSecurity *security_object, const uint32_t lifetime = 0) = 0;
 
     /**
+     * \brief Updates or refreshes the client's registration on the LWM2M
+     * server. Use this function to publish new objects to LWM2M server.
+     * \param security_object The security object from which the device object
+     * needs to update the registration. If there is only one LWM2M server registered,
+     * this parameter can be NULL.
+     * \param object_list Objects that contain information about the
+     * client attempting to register to the LWM2M server.
+     * \param lifetime The lifetime of the endpoint client in seconds. If the same value
+     * has to be passed, set the default value to 0.
+     */
+    virtual void update_registration(M2MSecurity *security_object, const M2MObjectList &object_list,
+                                     const uint32_t lifetime = 0) = 0;
+
+    /**
      * \brief Unregisters the registered object from the LWM2M server.
      * \param security_object The security object from which the device object
      * needs to be unregistered. If there is only one LWM2M server registered,

source/include/eventdata.h
@@ -86,6 +86,7 @@ public:
     virtual ~M2MUpdateRegisterData() {}
     M2MSecurity     *_object;
     uint32_t        _lifetime;
+    M2MObjectList    _object_list;
 };
 
 

source/include/m2minterfaceimpl.h
@@ -29,7 +29,7 @@ class M2MConnectionHandler;
 class M2MConnectionSecurity;
 class EventData;
 class M2MTimer;
-
+class M2MUpdateRegisterData;
 /**
  *  @brief M2MInterfaceImpl.
  *  This class implements handling of all mbed Client Interface operations
@@ -122,6 +122,20 @@ public:
     virtual void update_registration(M2MSecurity *security_object, const uint32_t lifetime = 0);
 
     /**
+     * @brief Updates or refreshes the client's registration on the LWM2M
+     * server. Use this function to publish new objects to LWM2M server.
+     * @param security_object The security object from which the device object
+     * needs to update the registration. If there is only one LWM2M server registered,
+     * this parameter can be NULL.
+     * @param object_list Objects that contain information about the
+     * client attempting to register to the LWM2M server.
+     * @param lifetime The lifetime of the endpoint client in seconds. If the same value
+     * has to be passed, set the default value to 0.
+     */
+    virtual void update_registration(M2MSecurity *security_object, const M2MObjectList &object_list,
+                                     const uint32_t lifetime = 0);
+
+    /**
      * @brief Unregisters the registered object from the LWM2M server
      * @param security_object, Security object from which the device object
      * needs to be unregistered, if there is only one LWM2M server registered
@@ -146,7 +160,7 @@ public:
      */
     virtual void set_platform_network_handler(void *handler = NULL);
 
-/**
+    /**
      * \brief Sets the function callback that will be called by mbed-client for
      * fetching random number from application for ensuring strong entropy.
      * \param random_callback A function pointer that will be called by mbed-client
@@ -304,6 +318,11 @@ private: // state machine state functions
     * When the client is waiting to receive or send data.
     */
     void state_waiting( EventData *data);
+
+    /**
+     * Start registration update.
+     */
+    void start_register_update(M2MUpdateRegisterData *data);
 
     /**
     * State enumeration order must match the order of state

source/m2minterfaceimpl.cpp
@@ -205,49 +205,23 @@ void M2MInterfaceImpl::register_object(M2MSecurity *security, const M2MObjectLis
 
 void M2MInterfaceImpl::update_registration(M2MSecurity *security_object, const uint32_t lifetime)
 {
-    tr_debug("M2MInterfaceImpl::update_registration - IN");
-    // Transition to a new state based upon
-    // the current state of the state machine
-    if(lifetime != 0 && (lifetime < MINIMUM_REGISTRATION_TIME)) {
-        _observer.error(M2MInterface::InvalidParameters);
-    } else if(!_update_register_ongoing){
-        tr_debug("M2MInterfaceImpl::update_registration - already ongoing");
-        _update_register_ongoing = true;
-        M2MUpdateRegisterData *data = new M2MUpdateRegisterData();
-        data->_object = security_object;
-        data->_lifetime = lifetime;
-        BEGIN_TRANSITION_MAP                                    // - Current State -
-            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state_idle
-            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state_bootstrap
-            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state__bootstrap_address_resolved
-            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state_bootstrap_resource_created
-            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state_bootstrapped
-            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state_register
-            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state_register_address_resolved
-            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state_register_resource_created
-            TRANSITION_MAP_ENTRY (STATE_UPDATE_REGISTRATION)    // state_registered
-            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state_update_registration
-            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state_unregister
-            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state_unregistered
-            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state_sending_coap_data
-            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state_coap_data_sent
-            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state_coap_data_received
-            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state_processing_coap_data
-            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state_coap_data_processed
-            TRANSITION_MAP_ENTRY (STATE_UPDATE_REGISTRATION)    // state_waiting
-        END_TRANSITION_MAP(data)
-        if(_event_ignored) {
-            _event_ignored = false;
-            if (!_reconnecting)
-                _observer.error(M2MInterface::NotAllowed);
-        }
-    } else if(!_reconnecting) {
-        tr_debug("M2MInterfaceImpl::update_registration - NOT ALLOWED");
-        _observer.error(M2MInterface::NotAllowed);
-    } else {
-        tr_debug("M2MInterfaceImpl::update_registration - reconnecting");
-    }
-    tr_debug("M2MInterfaceImpl::update_registration - OUT");
+    tr_debug("M2MInterfaceImpl::update_registration(M2MSecurity *security_object, const uint32_t lifetime)");
+    M2MUpdateRegisterData *data = new M2MUpdateRegisterData();
+    data->_object = security_object;
+    data->_lifetime = lifetime;
+    start_register_update(data);
+}
+
+void M2MInterfaceImpl::update_registration(M2MSecurity *security_object,
+                                           const M2MObjectList &object_list,
+                                           const uint32_t lifetime)
+{
+    tr_debug("M2MInterfaceImpl::update_registration(M2MSecurity *security_object, const M2MObjectList &object_list, const uint32_t lifetime)");
+    M2MUpdateRegisterData *data = new M2MUpdateRegisterData();
+    data->_object = security_object;
+    data->_lifetime = lifetime;
+    data->_object_list = object_list;
+    start_register_update(data);
 }
 
 void M2MInterfaceImpl::unregister_object(M2MSecurity* /*security*/)
@@ -780,12 +754,15 @@ void M2MInterfaceImpl::state_registered( EventData */*data*/)
     _reconnecting = false;
 }
 
-void M2MInterfaceImpl::state_update_registration( EventData *data)
+void M2MInterfaceImpl::state_update_registration(EventData *data)
 {
     tr_debug("M2MInterfaceImpl::state_update_registration");
-    // Start with registration preparation
     if(data) {
         M2MUpdateRegisterData *event = static_cast<M2MUpdateRegisterData *> (data);
+        // Create new resources if any
+        if (!event->_object_list.empty()) {
+            _nsdl_interface->create_nsdl_list_structure(event->_object_list);
+        }
         _nsdl_interface->send_update_registration(event->_lifetime);
     }
 }
@@ -994,5 +971,51 @@ void M2MInterfaceImpl::state_function( uint8_t current_state, EventData* data )
         case STATE_WAITING:
             M2MInterfaceImpl::state_waiting(data);
             break;
+    }
+}
+
+void M2MInterfaceImpl::start_register_update(M2MUpdateRegisterData *data) {
+    tr_debug("M2MInterfaceImpl::start_register_update - IN");
+    bool started = false;
+    if(!data || (data->_lifetime != 0 && (data->_lifetime < MINIMUM_REGISTRATION_TIME))) {
+        _observer.error(M2MInterface::InvalidParameters);
+    } else if(!_update_register_ongoing){
+        tr_debug("M2MInterfaceImpl::start_register_update - already ongoing");
+        started = true;
+        _update_register_ongoing = true;
+        BEGIN_TRANSITION_MAP                                    // - Current State -
+            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state_idle
+            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state_bootstrap
+            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state__bootstrap_address_resolved
+            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state_bootstrap_resource_created
+            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state_bootstrapped
+            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state_register
+            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state_register_address_resolved
+            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state_register_resource_created
+            TRANSITION_MAP_ENTRY (STATE_UPDATE_REGISTRATION)    // state_registered
+            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state_update_registration
+            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state_unregister
+            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state_unregistered
+            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state_sending_coap_data
+            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state_coap_data_sent
+            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state_coap_data_received
+            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state_processing_coap_data
+            TRANSITION_MAP_ENTRY (EVENT_IGNORED)                // state_coap_data_processed
+            TRANSITION_MAP_ENTRY (STATE_UPDATE_REGISTRATION)    // state_waiting
+        END_TRANSITION_MAP(data)
+        if(_event_ignored) {
+            _event_ignored = false;
+            if (!_reconnecting)
+                _observer.error(M2MInterface::NotAllowed);
+        }
+    } else if(!_reconnecting) {
+        tr_debug("M2MInterfaceImpl::start_register_update - NOT ALLOWED");
+        _observer.error(M2MInterface::NotAllowed);
+    } else {
+        tr_debug("M2MInterfaceImpl::start_register_update - reconnecting");
+    }
+
+    if(!started && data) {
+        delete data;
     }
 }

test/mbedclient/utest/m2minterfaceimpl/test_m2minterfaceimpl.cpp
@@ -422,6 +422,29 @@ void Test_M2MInterfaceImpl::test_update_registration()
     impl->update_registration(NULL,120);
     CHECK(observer->error_occured == true);
 
+
+    M2MObject *object = new M2MObject("test");
+    M2MObjectInstance *ins = object->create_object_instance();
+    ins->create_dynamic_resource("test","type",M2MResourceInstance::STRING,false,false);
+
+    M2MObjectList list;
+    list.push_back(object);
+    impl->_update_register_ongoing = false;
+    impl->_current_state = M2MInterfaceImpl::STATE_REGISTERED;
+    m2mnsdlinterface_stub::bool_value = false;
+    impl->update_registration(NULL, list);
+
+    CHECK(impl->_current_state == M2MInterfaceImpl::STATE_UPDATE_REGISTRATION);
+    list.clear();
+
+    impl->_current_state = M2MInterfaceImpl::STATE_REGISTERED;
+    impl->_update_register_ongoing = false;
+    m2mnsdlinterface_stub::bool_value = false;
+    impl->update_registration(NULL, list);
+
+    CHECK(impl->_current_state == M2MInterfaceImpl::STATE_UPDATE_REGISTRATION);
+    delete object;
+
 }
 
 void Test_M2MInterfaceImpl::test_unregister_object()

test/mbedclient/utest/stub/m2minterfaceimpl_stub.cpp
@@ -62,6 +62,11 @@ void M2MInterfaceImpl::register_object(M2MSecurity *, const M2MObjectList &)
 void M2MInterfaceImpl::update_registration(M2MSecurity *, const uint32_t)
 {
 }
+void M2MInterfaceImpl::update_registration(M2MSecurity *,
+                                           const M2MObjectList &,
+                                           const uint32_t)
+{
+}
 
 void M2MInterfaceImpl::unregister_object(M2MSecurity*)
 {

commit 9011c82222674dc7501339e0244494717cf4ed71

Fix regression caused by https://github.com/ARMmbed/mbed-client/pull/288

source/m2mnsdlinterface.cpp
@@ -297,7 +297,8 @@ bool M2MNsdlInterface::send_update_registration(const uint32_t lifetime)
     }
     tr_debug("M2MNsdlInterface::send_update_registration( lifetime %" PRIu32 ")", lifetime);
     bool success = false;
-
+    create_nsdl_list_structure(_object_list);
+    
     //If Lifetime value is 0, then don't change the existing lifetime value
     if(lifetime != 0) {
         if(_endpoint->lifetime_ptr) {