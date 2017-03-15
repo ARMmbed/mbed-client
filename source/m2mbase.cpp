950afc71364c5b24c64f1ce12434fb91f75cae3f
Tero Jääskö <tero.jaasko@arm.com>
2017-03-15 09:55:47 +0200

    m2mbase: remove set_observation_numer() which has been deprecated
    Remove the set_observation_number() which has been deprecated and
    nonfunctional for over a year.

--- source/m2mbase.cpp
+++ source/m2mbase.cpp
@@ -333,10 +333,6 @@ void M2MBase::set_instance_id(const uint16_t inst_id)
     _sn_resource->instance_id = inst_id;
 }
 
-void M2MBase::set_observation_number(const uint16_t /*observation_number*/)
-{
-}
-
 void M2MBase::set_max_age(const uint32_t max_age)
 {
     _sn_resource->max_age = max_age;
