eecb41bb8a1a4bef27c3cbe30e832889e515a5b9
Tommi Pihlajaniemi <Tommi.Pihlajaniemi@arm.com>
2017-03-30 10:45:25 +0300

    Restore nsdl_c allocator-wrappers

--- source/include/nsdlaccesshelper.h
+++ source/include/nsdlaccesshelper.h
@@ -28,6 +28,8 @@ uint8_t __nsdl_c_callback(struct nsdl_s * nsdl_handle,
                           sn_coap_hdr_s *received_coap_ptr,
                           sn_nsdl_addr_s *address,
                           sn_nsdl_capab_e nsdl_capab);
+void *__nsdl_c_memory_alloc(uint16_t size);
+void __nsdl_c_memory_free(void *ptr);
 uint8_t __nsdl_c_send_to_server(struct nsdl_s * nsdl_handle,
                                 sn_nsdl_capab_e protocol,
                                 uint8_t *data_ptr,
